2072bf16151bdb16cd6e3f9808fa8b9454363ee9
huasuhui <1067897525@qq.com>
2016-09-07 23:17:43 +0800

    add Dao

---
+++ com/suifengwiki/Dao/ExeSQL.java
@@ -0,0 +1,33 @@
+package com.suifengwiki.Dao;
+
+public class ExeSQL {
+	
+	public boolean insert(String sql){
+		return false;
+	}
+	
+	public boolean delete(String sql){
+		return false;
+	}
+	
+	public boolean update(String sql){
+		return false;
+	}
+	
+	public boolean query(String sql){
+		return false;
+	}
+	
+	public void getOneValue(String sql){
+		
+	}
+
+	public ExeSQL() {
+		
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ com/suifengwiki/Dao/SSRS.java
@@ -0,0 +1,29 @@
+package com.suifengwiki.Dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 此工具类用作存储从数据库取出的数据
+ * */
+public class SSRS {
+	
+	private int row = 0;
+	private int col = 0;
+	
+	private List<String> rows = new ArrayList<String>();
+	private List<String> cols = new ArrayList<String>();
+	
+	private void addData(){
+		
+	}
+	
+	private List<List<String>> addListData(){
+		return null;
+	}
+
+	public SSRS() {
+		
+	}
+
+}
